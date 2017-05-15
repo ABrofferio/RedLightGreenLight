7a7db53e3ba154caa9108da44b2af8256370c97a
Aja Brofferio <aja.brofferio@gmail.com>
2017-05-14 18:33:30 -0700

    adding randomization to the speeds of the animations and adding a reset button

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 #RedLightGreenLight
+
+This is a game of redlight greenlight built with javascript that allows the user to manipulate movement of cars using
+animations based on whether the traffic light is red or green.
+
+This project is currently incomplete.
+
+Things to do:
+-Declare winner
+-Changing styling of traffic light
--- redgreen.js
+++ redgreen.js
@@ -1,47 +1,13 @@
-/*let mycar = document.getElementById("car1"); 
-let pos = 0;
-
-function myMove(elem, pos) {
-    let id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 97) {
-            clearInterval(id);
-        } else if (($('body').style.backgroundColor.value ==='green')){
-            pos++; 
-            elem.style.left = pos + '%'; 
-        }
-    }
-}
-
 $('body').on('keydown', function(){
+    let speed1 = Math.floor((Math.random() * 150));
+    let speed2 = Math.floor((Math.random() * 150));
+    let speed3 = Math.floor((Math.random() * 150));
+    let speed4 = Math.floor((Math.random() * 150));
     this.style.backgroundColor = "green";
-    myMove(mycar,0);
-});
-$('body').on('keyup',function(){
-    this.style.backgroundColor = "red";
-});*/
-
-/*let mycar = document.getElementById("car1"); 
-let pos = 0;
-
-function myMove(elem, pos) {
-    let id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 97) {
-            clearInterval(id);
-        } else if (($('body').style.backgroundColor.value ==='green')){
-            pos++; 
-            elem.style.left = pos + '%'; 
-        }
-    }
-}*/
-
-$('body').on('keydown', function(){
-    this.style.backgroundColor = "green";
-    $('#car1').animate({ left: "+=5px" }, 100 );
-    $('#car2').animate({ left: "+=5px" }, 400 );
-    $('#car3').animate({ left: "+=5px" }, 400 );
-    $('#car4').animate({ left: "+=5px" }, 800 );
+    $('#car1').animate({ left: "+=20px" }, speed1);
+    $('#car2').animate({ left: "+=20px" }, speed2);
+    $('#car3').animate({ left: "+=20px" }, speed3);
+    $('#car4').animate({ left: "+=20px" }, speed4);
 });
 $('body').on('keyup',function(){
     this.style.backgroundColor = "red";
@@ -49,4 +15,11 @@ $('body').on('keyup',function(){
     $('#car2').stop();
     $('#car3').stop();
     $('#car4').stop();
+});
+
+$('button').on('click', function(){
+    $('#car1').css({ "left": "0%"});
+    $('#car2').css({ "left": "0%"});
+    $('#car3').css({ "left": "0%"});
+    $('#car4').css({ "left": "0%"});
 });
